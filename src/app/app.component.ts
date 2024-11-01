2b6ba4f9a36cd6b99a03d9f877f75f99d2951a24
RamiroRomera <405343@tecnicatura.frc.utn.edu.ar>
2024-11-01 12:27:16 -0300

    New Navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { ToastsContainer } from 'ngx-dabd-grupo01';
+import {MainLayoutComponent, NavbarItem, ToastsContainer} from 'ngx-dabd-grupo01';
 import { SessionService } from './services/session.service';
 import {AsyncPipe, CommonModule} from '@angular/common';
 import {SidebarComponent} from "./components/common/sidebar/sidebar.component";
@@ -13,12 +13,53 @@ import {LoginComponent} from "./components/repositories/users/login/login.compon
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, ToastsContainer, SidebarComponent, NavbarComponent,
-    RouterOutlet, AsyncPipe, CommonModule, RouterLink, LoginComponent],
+    RouterOutlet, AsyncPipe, CommonModule, RouterLink, LoginComponent, MainLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'users-cadastre-addresses-TPI';
+
+  navbarMenu: NavbarItem[] = [
+    {
+      label: 'Usuarios',
+      sidebarMenu: [
+        {
+          label: 'Propietarios',
+          subMenu: [
+            { label: 'Lista de Propietarios', routerLink: '/owner/list' },
+            { label: 'Cargar Propietario', routerLink: '/owner/form' },
+            { label: 'Cargar Archivo', routerLink: '/files/form' },
+            { label: 'Validar Archivos', routerLink: '/files/view' },
+          ]
+        },
+        {
+          label: 'Lotes',
+          subMenu: [
+            { label: 'Lista de Lotes', routerLink: '/plot/list' },
+            { label: 'Cargar Lote', routerLink: '/plot/form' },
+          ]
+        },
+        {
+          label: 'Usuarios',
+          subMenu: [
+            { label: 'Lista de Usuarios', routerLink: '/user/list' },
+            { label: 'Cargar Usuario', routerLink: '/user/form' },
+            { label: 'Cargar Usuario Inquilino', routerLink: '/user/tenant/form' },
+            { label: 'Lista de Roles', routerLink: '/roles/list' },
+            { label: 'Cargar Roles', routerLink: '/roles/form' },
+          ]
+        },
+        {
+          label: 'Cuentas',
+          subMenu: [
+            { label: 'Lista de Cuentas', routerLink: '/account/list' },
+          ]
+        }
+      ]
+    }
+  ];
+
 
   /**
    * Service responsible for managing user session and authentication state.
