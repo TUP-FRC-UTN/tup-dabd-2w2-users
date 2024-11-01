aaae653f371a6dc46ef1e25d78cc0b47384edb25
RamiroRomera <405343@tecnicatura.frc.utn.edu.ar>
2024-11-01 15:00:43 -0300

    Title for report

--- src/app/components/repositories/users/users/users-created-by-user/users-created-by-user.component.ts
+++ src/app/components/repositories/users/users/users-created-by-user/users-created-by-user.component.ts
@@ -1,9 +1,9 @@
 import {Component, inject} from '@angular/core';
-import {MainContainerComponent} from "ngx-dabd-grupo01";
+import {MainContainerComponent, ToastService} from "ngx-dabd-grupo01";
 import {UserService} from "../../../../../services/user.service";
 import {User} from "../../../../../models/user";
 import {NgClass} from "@angular/common";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {routes} from "../../../../../app.routes";
 import {NgbPagination} from "@ng-bootstrap/ng-bootstrap";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
@@ -24,26 +24,38 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 export class UsersCreatedByUserComponent {
   private router = inject(Router)
   private userService = inject(UserService)
+  private activatedRoute = inject(ActivatedRoute);
+  private toastService = inject(ToastService)
 
   users!: User[]
+  userName!: string;
 
   //#region ATT de PAGINADO
   currentPage: number = 0
   pageSize: number = 9
   sizeOptions : number[] = [3, 6, 9]
-  usersList: User[] = [];
-  filteredUsersList: User[] = [];
   lastPage: boolean | undefined
   totalItems: number = 0;
   //#endregion
 
   ngOnInit() {
-    this.userService.getAllUsers(this.currentPage, this.pageSize, true).subscribe({
-      next: result => {
-        this.users = result.content;
-        this.totalItems = result.totalElements;
-      }
-    })
+    const id = this.activatedRoute.snapshot.params['id'];
+
+    if (id) {
+      this.userService.getUserById(id).subscribe({
+        next: result => {
+          this.userName = result.firstName + " " + result.lastName
+        }
+      })
+      this.userService.getAllUsers(this.currentPage, this.pageSize, true).subscribe({
+        next: result => {
+          this.users = result.content;
+          this.totalItems = result.totalElements;
+        }
+      })
+    } else {
+      this.toastService.sendError("Error al cargar la pagina, reintente.")
+    }
   }
 
   redirectToDetail(id?: number) {
